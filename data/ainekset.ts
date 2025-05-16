57a98f357572e5755be86f1dcfd691ff34fefcf0
Kalmari87 <kalle.laakso1987@gmail.com>
2025-05-16 14:56:35 +0300

    Create ainekset.ts

---
+++ data/ainekset.ts
@@ -0,0 +1,25 @@
+const aineksetLista = [
+  "peruna",
+  "pasta",
+  "riisi",
+  "jauheliha",
+  "kana",
+  "tomaatti",
+  "tomaattimurska",
+  "sipuli",
+  "valkosipuli",
+  "kerma",
+  "juusto",
+  "munat",
+  "maito",
+  "voi",
+  "öljy",
+  "herne",
+  "paprika",
+  "kurkku",
+  "salaatti",
+  "leipä",
+  "pähkinät"
+];
+
+export default aineksetLista;
